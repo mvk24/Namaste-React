da72f266e2dc91370373173f3dcc2b05421e415f
mvk24 <varun.mithyantha@gmail.com>
2025-02-11 20:48:59 +0530

    Episode 1 : Part 2 Started

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,18 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "I am inside the child 1 tag"),
+    React.createElement("h2", {}, "I am inside the child 1 tag")
+], React.createElement("div", {
+    id: "child2"
+}, [
+    React.createElement("h1", {}, "I am inside the child 2 tag"),
+    React.createElement("h2", {}, "I am inside the child 2 tag")
+])));
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
